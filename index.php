8cf232ebbfc22dd27e1071ec79f1ea7b2b5af16d
SergeyAsadchiy <Sergey.Asadchiy@gmail.com>
2018-12-12 12:44:48 +0200

    Navibar

--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<a href="http://easy-it-courses.com/sumy/it-courses/PhpJunior">EasyIT</a><br>
-<a href="https://www.facebook.com/groups/189801585296608/?ref=bookmarks">Facebook: PHP Junior 30/11/18</a>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<ul class="navbar-nav mr-auto">
+		<li class="nav-item active">
+			<a class="nav-link" href="http://easy-it-courses.com/sumy/it-courses/PhpJunior">EasyIT</a>
+		</li>
+		<li class="nav-item active">	
+			<a class="nav-link" href="https://www.facebook.com/groups/189801585296608/?ref=bookmarks">Facebook:PHP</a>
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">GitHub</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          			<a class="dropdown-item" href="https://github.com/SergeyAsadchiy/PHP2">SergeyAsadchiy/PHP2</a>
+          			<a class="dropdown-item" href="https://github.com/vovadoma/PHP">vovadoma/PHP</a>
+          		</div>	
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">BootStrap</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          			<a class="dropdown-item" href="https://getbootstrap.com/docs/4.1/components/card/">карточки</a>
+          			<a class="dropdown-item" href="https://getbootstrap.com/docs/4.1/layout/grid/?fbclid=IwAR2FwsEGj6emC3MI01B05uVQKXFINLLRAI6aJzDOwPnsD0_c0dlBO3yt0Co">сетка</a>
+          			<a class="dropdown-item" href="https://getbootstrap.com/docs/4.1/components/navbar/">navbar</a>
+          		</div>	
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PHP</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          			<a class="dropdown-item" href="http://php.net/manual/ru/langref.php">php.net</a>
+          			<a class="dropdown-item" href="http://www.php.su/">php.su</a>
+          		</div>	
+		</li>
+		<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">вопросы на среду</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          			<a class="dropdown-item" href="#">Как лучше совмещать PHP код и HTML в одном файле (как код для вывода карточек или таблицы)?</a>
+          			<a class="dropdown-item" href="#">Можно ли часть HTML кода писать в другом файле. (Например - это меню, в нем совсем нет PHP кода)</a>
+          			<a class="dropdown-item" href="#">Можно ли запускать приложение Windows из браузера?</a>
+          		</div>	
+		</li>	
+	</ul>
+</nav>
+
 
 <?php
 
@@ -51,28 +93,30 @@ foreach ($items as $item) {
 
 // sabstitution of picrures
     foreach ($images as $image) {
-                if ($item['id']==$image['id']) {
-                    $image_card=$image['img'];
-                    break;
-                    //if (!isset($image['img'])) $image_card=$noImage;                    
-                } else {
-                    $image_card=$noImage;
-                }
-        }
+            if ($item['id']==$image['id']) {
+                $image_card=$image['img'];
+                break;
+                //if (!isset($image['img'])) $image_card=$noImage;                    
+            } else {
+                $image_card=$noImage;
+            }
+    } //конец цикла $images[]   
 ?>
-        <div class="col-sm-4">
-            <div class="card border-success mb-3" style="max-width: 18rem;">
-                <div class="card-header bg-transparent border-success">Колличество товара: <?php echo $item['stock'] ?></div>
-                <div class="card-body text-success">
-                    <h5 class="card-title"><?php echo $item['name'] ?></h5>
-                    <img class="card-img-top" src="<?php echo $image_card?>" alt="textNoImage">
-                    <p class="card-text"><?php echo $description ?></p>
-                </div>
-                <div class="card-footer bg-transparent border-success">Цена - <?php echo $price_disc ?> грн.</div>
+	<!--вывод карточки-->
+    <div class="col-sm-4">
+        <div class="card border-success mb-3" style="max-width: 18rem;">
+            <div class="card-header bg-transparent border-success">Колличество товара: <?php echo $item['stock'] ?></div>
+            <div class="card-body text-success">
+                <h5 class="card-title"><strong><?php echo $item['name'] ?></strong></h5>
+                <img class="card-img-top" src="<?php echo $image_card?>" alt="textNoImage">
+                <p class="card-text"><?php echo $description ?></p>
             </div>
-        </div>  
+            <div class="card-footer bg-transparent border-success">Цена - <strong><?php echo $price_disc ?></strong> грн.</div>
+        </div>
+    </div>  
 <?php
-}
+
+} //конец цикла $items[]
 echo'
     </div>
 </div> 
