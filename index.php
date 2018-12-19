bcebdeea96f7a9328c01b51d777b8385fde8b726
SergeyAsadchiy <Sergey.Asadchiy@gmail.com>
2018-12-20 00:42:24 +0200

    function cropString

--- index.php
+++ index.php
@@ -5,6 +5,30 @@ indexHome();
 //indexAdmin();
 
 
+$str='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit itaque, sequi accusamus molestias laudantium accusantium minus animi excepturi nobis esse ex recusandae maxime optio rem, neque amet voluptatibus, enim adipisci. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit itaque, sequi accusamus molestias laudantium accusantium minus animi excepturi nobis esse ex recusandae maxime optio rem, neque amet voluptatibus, enim adipisci.models/Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit itaque, sequi accusamus molestias laudantium accusantium minus animi excepturi nobis esse ex recusandae maxime optio rem, neque amet voluptatibus, enim adipisci.';
+$num='';
+
+//-----функция обрезает текст после заданного колличества символов. Текст обрезается после целого слова.
+function cropString($str,$limit=20) {     
+    //$lim = (is_numeric($limit)) ? $limit : 20;  // проверка на число. Если не число, то принимаем = 20                     
+    $arr = explode(' ', $str);                  // получение массива слов из строки. Разделители - "пробел".
+    $string = '';                               // инициализация суммирующей строки
+
+    foreach ($arr as $word) {
+        $string = $string.' '. $word;           // добавляем к строке пробел и слова для каждого элемента массива
+        if (strlen($string) >= $limit) {          // если длинна полученной строки больше заданной длинны
+            $outstring = $string.'...';         //      то добавляем к строке "..." 
+            break;                              //      и выходим из цикла
+        }                                       //
+        else  
+            $outstring = $string;               // иначе $outstring равно текущему значению строки (т.е если 
+        }                                       //      заданное число больше длинны строки, то выведется вся строка)
+    return $outstring;                          // результат
+}
+
+$result = cropString($str,$num);
+print_r($result);
+
 
 /* 
 //--------- Вариант с foreach через промежуточный массив $result. 
